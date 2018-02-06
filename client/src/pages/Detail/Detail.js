5ba2775851a587958a6ca7371e3dd17b334fb8f6
Jesse Bernier <jesse_bernier@hotmail.com>
2018-02-05 21:18:24 -0500

    error in Detals.js file, var name

--- client/src/pages/Detail/Detail.js
+++ client/src/pages/Detail/Detail.js
@@ -11,8 +11,11 @@ class Detail extends Component {
   // When this component mounts, grab the book with the _id of this.props.match.params.id
   // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   componentDidMount() {
-    API.getBook(this.props.match.params.id)
-      .then(res => this.setState({ book: res.data }))
+    API.getWordBank(this.props.match.params.id)
+      .then(res =>{
+        console.log(res.data);
+       this.setState({ word: res.data })
+     })
       .catch(err => console.log(err));
   }
 
@@ -23,7 +26,7 @@ class Detail extends Component {
           <Col size="md-12">
             <Jumbotron>
               <h1>
-                {this.state.book.title} by {this.state.book.author}
+                {this.state.word.title} by {/*{this.state.tags}*/}
               </h1>
             </Jumbotron>
           </Col>
@@ -33,7 +36,7 @@ class Detail extends Component {
             <article>
               <h1>Synopsis</h1>
               <p>
-                {this.state.book.synopsis}
+                {/*{this.state.word.description}*/}
               </p>
             </article>
           </Col>

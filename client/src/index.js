7fd43cb3d55b527d4f06c8bb1d3d5516fe332a3c
Kevin Gilland <kev.gilland93@gmail.com>
2018-02-12 19:16:05 -0500

    fixed error in src/index

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import hangmanApp from './reducers'
-import thunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux'
+// import hangmanApp from './reducers'
+// import thunk from 'redux-thunk'
+// import { createStore, applyMiddleware } from 'redux'
 
 
 
-let store = createStore(hangmanApp, applyMiddleware(thunk))
+// let store = createStore(hangmanApp, applyMiddleware(thunk))
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
